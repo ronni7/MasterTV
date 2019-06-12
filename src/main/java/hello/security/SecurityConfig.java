843bff523ba9af1974886a0e8e693b3219a55f2d
ronni7 <aomab77@gmail.com>
2019-06-12 17:30:02 +0200

    Fixed security problems

--- src/main/java/hello/security/SecurityConfig.java
+++ src/main/java/hello/security/SecurityConfig.java
@@ -1,17 +1,11 @@
 package hello.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@EnableWebSecurity
-@Configuration
+//@EnableWebSecurity
+//@Configuration
 public class SecurityConfig {
 
-
+/*
     @Configuration
     public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
@@ -33,5 +27,5 @@ public class SecurityConfig {
                     .httpBasic();
         }
 
-    }
+    }*/
 }
